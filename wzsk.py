1392ae53e321707281ee5628717980f112821439
Zhen Tian <zhen.tian@thoughtworks.com>
2019-08-10 22:06:39 +0800

    fix: validate frame

--- wzsk.py
+++ wzsk.py
@@ -31,7 +31,7 @@ class WZSK:
     @staticmethod
     def is_valid_frame(frame):
         checksum = ord(frame[-1])
-        expected_checksum = sum(ord(b) for b in frame[:-1]) ^ 0xff | 0x01
+        expected_checksum = ~sum(ord(b) for b in frame[:-1]) & 0xff | 0x01
         print('expected: {}, actual: {}'.format(expected_checksum, checksum))
         return checksum == expected_checksum
 
