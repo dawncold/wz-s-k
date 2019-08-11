5a1cd8abbf640046b7a4708f84619c044298a3c6
Zhen Tian <zhen.tian@thoughtworks.com>
2019-08-11 14:01:07 +0800

    debug

--- wzsk.py
+++ wzsk.py
@@ -18,7 +18,8 @@ class WZSK:
         self.setup_serial()
 
     def setup_serial(self):
-        self.serial = serial.Serial(port=self.serial_device, baudrate=9600, write_timeout=0.5)
+        self.serial = serial.Serial(port=self.serial_device, baudrate=9600, timeout=0.5,
+                                    write_timeout=0.5)
 
     def switch_to_passive_mode(self):
         print('switch to passive mode')
