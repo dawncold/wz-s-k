d30c03adfe4a7f0ea9bb2fcca940eb6a23f4c884
Zhen Tian <zhen.tian@thoughtworks.com>
2020-03-30 23:48:17 +0800

    feat: display with ams

---
+++ ams.py
@@ -0,0 +1,68 @@
+# -*- coding:utf-8 -*-
+
+import pigpio
+import time,sys,datetime
+
+pi = pigpio.pi()
+h = pi.i2c_open(1, 0x5a)
+
+"""
+return None
+
+return {
+    status: "OK",
+    values: [
+        {
+            name: "eco2",
+            unit: "ppm",
+            value: ...
+        },
+        {
+            name: "resistance",
+            unit: "ohm",
+            value: ...
+        },
+        {
+            name: "etvoc",
+            unit: "ppb",
+            value: ...
+        }
+    ]
+}
+"""
+
+def read():
+    try:
+        c, d = pi.i2c_read_device(h, 9)
+        length=len(d)
+        if length != 9:
+            return None
+        status = read_status(d[2])
+        ret_obj = dict(status=status, values=[])
+        prediction = d[0] * pow(2, 8) + d[1]
+        if d[2] == 0x01:
+            prediction=d[0]*pow(2,8)+d[1]
+        ret_obj['values'].append(dict(name='eCO2', unit='ppm', value=prediction))
+        if status == 'OK':
+            resistance = d[4]*pow(2,16)+d[5]*pow(2,8)+d[6]
+            ret_obj['values'].append(dict(name='resistance', unit='Ohm', value=resistance))
+            eTVOC= d[7]*pow(2,8)+d[8]
+            ret_obj['values'].append(dict(name='eTVOC', unit='ppb', value=eTVOC))
+        
+        return ret_obj
+    finally:
+        pi.i2c_close(h)
+        pi.stop()
+
+def read_status(val):
+    if val == 0:
+        return 'OK'
+    if val == 0x01:
+        return 'BUSY'
+    if val == 0x10:
+        return 'RUNIN'
+    if val == 0x80:
+        return 'BUSY'
+
+if __name__ == '__main__':
+    print(read())
--- display.py
+++ display.py
@@ -2,6 +2,7 @@
 
 import time
 from wzsk import WZSK
+from ams import read as ams_read
 
 
 def main(epaper):
@@ -38,26 +39,38 @@ def main(epaper):
         try:
             frame = device.get_frame()
         except:
-            epaper.clear(epaper.WHITE)
-            epaper.flush(epaper.PART)
-            epaper.setTextCursor(0, 10)
-            epaper.printStrLn('Get frame error, retry...')
-            epaper.flush(epaper.PART)
-            continue
-
-        if frame:
-            if WZSK.is_valid_frame(frame):
+            content1 = 'CH2O: -- µg/m³'
+            content2 = 'CH2O: -- ppb'
+        else:
+            if frame and WZSK.is_valid_frame(frame):
                 content1 = 'CH2O: {} µg/m³'.format(WZSK.calculate(frame[1], frame[2]))
                 content2 = 'CH2O: {} ppb'.format(WZSK.calculate(*device.get_value_high_and_low(frame)))
-
-                print(content1)
-                print(content2)
-
-                epaper.clear(epaper.WHITE)
-                epaper.flush(epaper.PART)
-                epaper.setTextCursor(0, 10)
-                epaper.printStrLn(content1)
-                epaper.printStrLn(content2)
-                epaper.flush(epaper.PART)
-
+            else:
+                content1 = 'CH2O: -- µg/m³'
+                content2 = 'CH2O: -- ppb'
+        
+        try:
+            ams_result = ams_read()
+        except:
+            content3 = 'eCO2: -- ppm'
+            content4 = 'eTVOC: -- ppb'
+        else:
+            if ams_result['status'] == 'OK':
+                name2value = {e['name']: e for e in ams_result['values']}
+                eco2_result = name2value.get('eCO2')
+                content3 = 'eCO2: {} ppm'.format(eco2_result['value'] if eco2_result else '--')
+                etvoc_result = name2value.get('eTVOC')
+                content3 = 'eTVOC: {} ppb'.format(etvoc_result['value'] if etvoc_result else '--')
+            else:
+                content3 = 'eCO2: -- ppm'
+                content4 = 'eTVOC: -- ppb'
+                
+        epaper.clear(epaper.WHITE)
+        epaper.flush(epaper.PART)
+        epaper.setTextCursor(0, 10)
+        epaper.printStrLn(content1)
+        epaper.printStrLn(content2)
+        epaper.printStrLn(content3)
+        epaper.printStrLn(content4)
+        epaper.flush(epaper.PART)
         time.sleep(5)
